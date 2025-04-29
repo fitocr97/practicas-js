cab3694cde13488657016318619686046280e8d9
Rodolfo Mora <rodolfomora97@outlook.com>
2025-04-29 17:09:44 -0600

    4 practicas faciles

---
+++ js/day1.js
@@ -0,0 +1,52 @@
+/*
+Reto 1: Saludo personalizado
+Pedí al usuario su nombre con prompt().
+
+Mostrá en consola:
+"¡Hola, [nombre]! Bienvenido a JavaScript."
+*/
+/*
+const nombre = prompt(' cual es tu nombre:' )
+
+console.log(`¡Hola, ${nombre}! Bienvenido a JavaScript`)
+*/
+
+/*
+    Reto 2: Calculadora básica
+    Pedí dos números.
+    Mostrá la suma, resta, multiplicación y división de ambos.
+*/
+/*
+const numero1 = prompt('numero 1:' )
+const numero2 = prompt('numero 2:' )
+const suma = numero1 + numero2;
+console.log(`suma: ${suma} , resta:${numero1 - numero2} , multiplicacion: ${numero1 * numero2} , divicion: ${numero1 / numero2} `)
+*/
+
+/*
+    Reto 3: Número par o impar
+    Pedí un número.
+    Mostrá si es par o impar usando %.
+*/
+/*
+const numero3 = prompt('numero 3:' )
+if(numero3%2 ===0){
+    console.log('es par')
+}else{
+    console.log('es impar')
+}
+*/
+
+/*
+    Reto 4: Edad válida
+    Pedí al usuario su edad.
+    Si es menor de 0 o mayor a 120, mostrale "Edad no válida".
+    Si está entre 0 y 120, mostrale "Edad correcta".
+*/
+/*
+const edad = prompt('ingrese su edad');
+if(edad<120 && edad >0){
+    console.log('edad correcta')
+}else{
+    console.log('edad incorrecta')
+}*/
